2deeda01eb79c9d6291a7a9aa48c3ce654a50b90
Jian Wang <jian.wang82@gmail.com>
2022-05-21 22:52:46 +1000

    Test the conversion of a string with separator into a list of strings

---
+++ Common.UnitTests/String/CustomStringConverterTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Common.UnitTests.String
+{
+    public class CustomStringConverterTests
+    {
+        [Theory]
+        [InlineData("6 1 5 9 2", " ")]
+        public void ConvertStringWithSeparatorIntoStringList_ShouldReturnAListOfNumbers_InlineDataTests(string input, string delimiter)
+        {
+            // arrange
+            var sut = new CustomStringConverter();
+
+            // act
+            var result = sut.ConvertStringWithSeparatorIntoStringList(input, delimiter);
+
+            // assert
+            result.Should().NotBeNullOrEmpty();
+        }
+    }
+}
---
+++ Common/String/CustomStringConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.String
+{
+    public class CustomStringConverter
+    {
+        public IList<string> ConvertStringWithSeparatorIntoStringList(string input, string delimiter)
+        {
+            return input.Split(delimiter).ToList();
+        }
+    }
+}
